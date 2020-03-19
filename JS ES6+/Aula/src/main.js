23787ef7af3953fdf3be1d145669d153a133826f
Natan Barbosa <natanbarbosa15@gmail.com>
2020-03-19 18:35:05 -0300

    JS ES6+ Completo

--- JS ES6+/Aula/src/main.js
+++ JS ES6+/Aula/src/main.js
@@ -1,15 +1,92 @@
-import axios from 'axios';
+import api from './api';
 
-const buscaUsuario = async (usuario) => {
-    try {
-        var response = await axios.get(`https://api.github.com/users/${usuario}`);
-        console.log(response.data);
+class App {
+    constructor () {
+        this.repositories = [];
+
+        this.formEl = document.getElementById('repo-form');
+        this.inputEl = document.querySelector('input[name=repository]')
+        this.listEl = document.getElementById('repo-list');
+
+        this.registerHandlers();
     }
-    catch (error) {
-        console.log('Usuário não existe');
+
+    registerHandlers() {
+        this.formEl.onsubmit = (event) => this.addRepository(event);
+    }
+
+    setLoading(loading = true) {
+        if(loading === true) {
+            let loadingEl = document.createElement('span');
+            loadingEl.appendChild(document.createTextNode('Carregando'));
+            loadingEl.setAttribute('id', 'loading');
+
+            this.formEl.appendChild(loadingEl);
+        }
+        else {
+            document.getElementById('loading').remove();
+        }
+    }
+
+    async addRepository(event) {
+        event.preventDefault();
+
+        const repoInput = this.inputEl.value;
+
+        if(repoInput.length === 0) { return; }
+
+        this.setLoading();
+
+        try {
+            const response = await api.get(`/repos/${repoInput}`);
+
+            const { name, description, html_url, owner: { avatar_url } } = response.data;
+
+            this.repositories.push({
+                name,
+                description,
+                avatar_url,
+                html_url,
+            });
+            
+            this.render();
+        }
+        catch (error) {
+            alert('O repositório não existe!');
+        }
+
+        this.inputEl.value = '';
+
+        this.setLoading(false);
+    }
+
+    render() {
+        this.listEl.innerHTML = '';
+
+        this.repositories.forEach(repo => {
+            let imgEl = document.createElement('img');
+            imgEl.setAttribute('src', repo.avatar_url);
+
+            let titleEl = document.createElement('strong');
+            titleEl.appendChild(document.createTextNode(repo.name));
+
+            let descriptionEl = document.createElement('p');
+            descriptionEl.appendChild(document.createTextNode(repo.description));
+
+            let linkEl = document.createElement('a');
+            linkEl.setAttribute('target', '_blank');
+            linkEl.setAttribute('href', repo.html_url);
+            linkEl.appendChild(document.createTextNode('Acessar'));
+
+            let listItemEl = document.createElement('li');
+            listItemEl.appendChild(imgEl);
+            listItemEl.appendChild(titleEl);
+            listItemEl.appendChild(descriptionEl);
+            listItemEl.appendChild(linkEl);
+
+            this.listEl.appendChild(listItemEl);
+        })
     }
 }
 
-buscaUsuario('natannmb15');
-buscaUsuario('natannmb155');
-   
+new App();
